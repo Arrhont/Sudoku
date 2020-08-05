60a74dd13d9b69c38d11c54ebc34c5cd73359664
Евгений Селянин <arrhont.es@gmail.com>
2020-08-06 00:48:21 +0300

    validator

--- src/solver.js
+++ src/solver.js
@@ -1,3 +1,7 @@
+//import pull from 'lodash/pull';
+
+const pull = require('lodash/pull');
+
 const mock = [
   [2, 1, 5, 0, 6, 4, 7, 8, 9],
   [7, 0, 6, 1, 5, 8, 3, 0, 0],
@@ -10,7 +14,40 @@ const mock = [
   [2, 5, 0, 6, 4, 8, 0, 0, 0],
 ];
 
-function transformToTwoDimArr(quadrantsArr) {
+function getNumberByCoords(arr, x, y) {
+  const numberOfQuadrants = arr.length;
+  const dimensionSize = Math.sqrt(numberOfQuadrants);
+
+  if (x >= numberOfQuadrants || y >= numberOfQuadrants) {
+    throw new TypeError('Недопустимые координаты');
+  }
+
+  const quadrant = Math.floor(x / dimensionSize) * dimensionSize + Math.floor(y / dimensionSize);
+  const quadrantX = x % dimensionSize;
+  const quadrantY = y % dimensionSize;
+  const index = quadrantX * dimensionSize + quadrantY;
+
+  return arr[quadrant][index];
+}
+
+function createTwoDimArrFromQuadrants(quadrantsArr) {
+  const numberOfQuadrants = quadrantsArr.length;
+  const dimensionSize = Math.sqrt(numberOfQuadrants);
+
+  const twoDimArr = new Array(dimensionSize);
+
+  for (let i = 0; i < numberOfQuadrants ; twoDimArr[i++] = []);
+
+  for (let i = 0; i < numberOfQuadrants; i++) {
+    for (let j = 0; j < numberOfQuadrants; j++ ){
+      twoDimArr[i][j] = getNumberByCoords(quadrantsArr, i, j);
+    }
+  }
+
+  return twoDimArr;
+}
+
+function validateSudoku(quadrantsArr) {
   const numberOfQuadrants = quadrantsArr.length;
   const dimensionSize = Math.sqrt(numberOfQuadrants);
 
@@ -24,34 +61,44 @@ function transformToTwoDimArr(quadrantsArr) {
     }
   }
 
-  const twoDimArr = new Array(dimensionSize);
+  for (const quadrant of quadrantsArr) {
+    const zerolessArr = pull([...quadrant], 0);
+    const set = new Set(zerolessArr);
 
-  for (let i = 0; i < numberOfQuadrants ; twoDimArr[i++] = []);
-
-  function getNumberByCoords(arr, x, y) {
-    const numberOfQuadrants = arr.length;
-    const dimensionSize = Math.sqrt(numberOfQuadrants);
-
-    if (x >= numberOfQuadrants || y >= numberOfQuadrants) {
-      throw new TypeError('Недопустимые координаты');
+    if (set.size !== zerolessArr.length) {
+      return false;
     }
+  }
 
-    const quadrant = Math.floor(x / dimensionSize) * dimensionSize + Math.floor(y / dimensionSize);
-    const quadrantX = x % dimensionSize;
-    const quadrantY = y % dimensionSize;
-    const index = quadrantX * dimensionSize + quadrantY;
+  const twoDimArr = createTwoDimArrFromQuadrants(quadrantsArr);
 
-    return arr[quadrant][index];
+  for (const lineX of twoDimArr) {
+    const zerolessArr = pull([...lineX], 0);
+    const set = new Set(zerolessArr);
+
+    if (set.size !== zerolessArr.length) {
+      return false;
+    }
   }
 
   for (let i = 0; i < numberOfQuadrants; i++) {
-    for (let j = 0; j < numberOfQuadrants; j++ ){
-      twoDimArr[i][j] = getNumberByCoords(quadrantsArr, i, j);
-    }
-  }
+    const lineY = [];
 
-  return twoDimArr;
+    for (let j=0; j < numberOfQuadrants; j++) {
+      lineY.push(twoDimArr[i][j]);
+    }
+
+    const zerolessArr = pull([...lineY], 0);
+    const set = new Set(zerolessArr);
+
+    if (set.size !== zerolessArr.length) {
+      return false;
+    }
+
+    return true;
+  }
 
 }
 
-console.log(transformToTwoDimArr(mock));
+console.log(validateSudoku(mock));
+console.log(createTwoDimArrFromQuadrants(mock));
